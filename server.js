eb90de1b3e7812123fa746d9553b3fdf1580b338
azj210 <ajiang1098@gmail.com>
2021-05-08 10:26:15 -0400

    set up server and necessary modules along with frontend

---
+++ server.js
@@ -0,0 +1,31 @@
+
+const express = require("express");
+const app = express();
+
+
+const userRouter = require("./backend/api/users/user.router");
+const mediaRouter = require("./backend/api/media/media.router")
+const cors = require("cors");
+
+var corsOptions = {
+    origin: ['http://localhost:3000'],
+    credentials: true,
+};
+app.use(cors(corsOptions));
+
+//since user passes in json, we convert it into javascript object
+app.use(express.json());
+
+//if any requests come for _ data then we pass to /api/_ route
+app.use("/api/users", userRouter);
+app.use("/api/media", mediaRouter);
+
+server = require("http").createServer(app);
+
+server.listen(process.env.PORT || 4000, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log("App listening at http://%s:%s", host, port);
+});
+
+module.exports = app;
