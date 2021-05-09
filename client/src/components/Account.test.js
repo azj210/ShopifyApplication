5c56aa29f0586d99bb73c0dd37ad1718df44158e
azj210 <ajiang1098@gmail.com>
2021-05-09 02:39:31 -0400

    add reactdom

--- client/src/components/Account.test.js
+++ client/src/components/Account.test.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from 'react-router-dom';
 import { render } from "@testing-library/react";
 import { shallow, configure } from "enzyme";
