6b4f653c73516c35e9e070bf54e751f484862a52
azj210 <ajiang1098@gmail.com>
2021-05-09 21:09:03 -0400

    add methods

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ var corsOptions = {
 };
 app.use(cors(corsOptions));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 //since user passes in json, we convert it into javascript object
 app.use(express.json());
 
