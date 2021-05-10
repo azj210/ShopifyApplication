bf4221b7b1b0ff7c513ea4b9794a596821257766
azj210 <ajiang1098@gmail.com>
2021-05-09 21:17:43 -0400

    close function

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
+});
 
 //since user passes in json, we convert it into javascript object
 app.use(express.json());
